8ff5292fd46a5a520ad4e623eaa839bd69bd18cd
EyadAshraf0 <109422045+EyadAshraf0@users.noreply.github.com>
2022-07-30 10:25:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function playcodes(){
+    var codes = document.getElementById("codes").value;
+    document.getElementById("output").innerHTML = codes;
+}
+function removecodes(){
+    document.getElementById("output").innerHTML = "";
+}
